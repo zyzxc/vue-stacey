3b520b77c3180ea18a53d838afefbf659717259c
zxc <zhangxingcai@zysoft.com.cn>
2017-11-21 17:30:51 +0800

    vue组件开发

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Collapse from '@/components/collapse'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Router)
+Vue.use(ElementUI)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/Hello',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/collapse',
+      name: 'collapse',
+      component: Collapse
     }
   ]
 })
